22eb516b44eae246e37665f59dc9326c25f2dcf0
MINEDUCYT <MINEDUCYT@LAPTOP-2RS2E00C>
2023-09-20 17:35:55 -0600

    FIGLIO DI PUTTANA!

--- src/main/java/org/ripple/Main.java
+++ src/main/java/org/ripple/Main.java
@@ -4,6 +4,7 @@ import org.ripple.core.LogIn;
 
 public class Main {
     public static void main(String[] args) {
+        //PRUEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  
         LogIn login = new LogIn();
     }
 }
