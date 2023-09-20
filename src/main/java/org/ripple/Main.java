a2bfb908b213e6841b315fe0cdf9085d7102ed7a
Ragnar21-05 <cristopher.fernandez22@itca.edu.sv>
2023-09-20 07:15:37 -0600

    prueba

--- src/main/java/org/ripple/Main.java
+++ src/main/java/org/ripple/Main.java
@@ -4,6 +4,6 @@ import org.ripple.core.LogIn;
 
 public class Main {
     public static void main(String[] args) {
-        LogIn login = new LogIn();
+        // TODO code application logic here
     }
 }
