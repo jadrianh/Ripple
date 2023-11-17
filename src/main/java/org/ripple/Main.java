6ada83cbcfdb354bc6cb2952e7135c220c4b1417
jadrianh <jadrianhernandez13@gmail.com>
2023-11-16 21:37:18 -0600

    Update Repository

--- src/main/java/org/ripple/Main.java
+++ src/main/java/org/ripple/Main.java
@@ -1,14 +1,24 @@
 package org.ripple;
 
-import org.ripple.core.main.Home;
-import org.ripple.core.Settings;
-import org.ripple.core.start.LogIn;
+import org.ripple.connection.CConexion;
+import org.ripple.ui.AddContact;
+import org.ripple.ui.ContactProfile;
+import org.ripple.ui.LogIn;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.SQLException;
+import java.util.zip.CheckedInputStream;
 
 public class Main {
     public static void main(String[] args) {
         LogIn login = new LogIn();
-        
+
         CConexion conexion = new CConexion();
-        conexion.establecerConection();
+        try {
+            conexion.establecerConection();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error al conectar a la Base de Datos");
+        }
     }
 }
