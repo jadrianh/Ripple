949f1d3b42bbd8c51a720daf388ef394285f76c6
jadrianh <jadrianhernandez13@gmail.com>
2023-11-16 21:37:54 -0600

    Update Repository

--- src/main/java/org/ripple/Main.java
+++ src/main/java/org/ripple/Main.java
@@ -1,15 +1,10 @@
 package org.ripple;
 
 import org.ripple.connection.CConexion;
-import org.ripple.ui.AddContact;
-import org.ripple.ui.ContactProfile;
 import org.ripple.ui.LogIn;
 
 import javax.swing.*;
-import java.awt.*;
 import java.sql.SQLException;
-import java.util.zip.CheckedInputStream;
-
 public class Main {
     public static void main(String[] args) {
         LogIn login = new LogIn();
